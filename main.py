f4c3d54e00adf5baec7471bef6f55c9ac76f63ef
Sycinz <gronbosof@gmail.com>
2026-02-06 22:58:56 +0100

    Simple suricata logs parsing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    print("Initial commit!")
+# Simple IDS/IDR log parser
+import json
+from pprint import pprint
 
+# Handling suricata logs (eve.json) just for now
+def main():
+    with open("./logs/eve.json") as data:
+        log = json.load(data)
+        if "alert" in log:
+            pprint(log)
 
 main()
